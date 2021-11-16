d9cc9720d9a8d67f0cead6933bee73bce3873159
Web Dev Help <43485053+webdevohelp@users.noreply.github.com>
2021-11-16 15:29:22 +0530

    React Text Editor

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import About from './components/About';
+import Alert from './components/Alert';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [mode, setMode] = useState('light');
+    const [alert, setAlert] = useState(null);
+
+    const showAlert = (message, type) => {
+        setAlert({
+            msg: message,
+            type: type,
+        });
+        setTimeout(() => {
+            setAlert(null);
+        }, 1500);
+    };
+
+    const handleMode = () => {
+        if (mode === 'light') {
+            setMode('dark');
+            document.body.style.backgroundColor = 'rgb(0, 0, 0)';
+            showAlert('Dark mode has been enabled', 'success');
+        } else {
+            setMode('light');
+            document.body.style.backgroundColor = 'white';
+            showAlert('Light mode has been enabled', 'success');
+        }
+    };
+    return (
+        <React.Fragment>
+            <BrowserRouter>
+                <Navbar
+                    title="Text Utils"
+                    mode={mode}
+                    handleMode={handleMode}
+                    aboutText="About Us"
+                />
+                <Alert alert={alert} />
+                <Routes>
+                    {/* <About />
+                    <TextForm /> */}
+                    <Route path="about" element={<About />} />
+                    <Route
+                        path=""
+                        element={
+                            <TextForm
+                                showAlert={showAlert}
+                                heading="Try TextUtils - word counter, character counter, remove extra spaces"
+                                mode={mode}
+                            />
+                        }
+                    />
+                </Routes>
+            </BrowserRouter>
+        </React.Fragment>
+    );
 }
 
 export default App;
---
+++ src/components/TextForm.js
@@ -0,0 +1,130 @@
+import React, { useState } from 'react';
+
+function TextForm(props) {
+    const [text, setText] = useState('');
+
+    const changeHandler = (e) => {
+        setText(e.target.value);
+    };
+
+    const handleUpClick = () => {
+        setText(text.toUpperCase());
+        props.showAlert('Converted to UpperCase!', 'success');
+    };
+
+    const handleLowClick = () => {
+        setText(text.toLowerCase());
+        props.showAlert('Converted to LowerCase!', 'success');
+    };
+
+    const handleClearClick = (e) => {
+        setText('');
+        props.showAlert('TextBox Cleared!', 'success');
+    };
+
+    const handleCopy = () => {
+        navigator.clipboard.writeText(text);
+        props.showAlert('Text Copied!', 'success');
+    };
+
+    const handleExtraSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(' '));
+        props.showAlert('Extra spaces removed!', 'success');
+    };
+
+    const handleRemoveSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(''));
+        props.showAlert('All spaces removed!', 'success');
+    };
+
+    return (
+        <React.Fragment>
+            <div
+                className="container py-3"
+                style={{
+                    backgroundColor:
+                        props.mode === 'dark' ? 'rgb(33,37,41)' : 'white',
+                    color: props.mode === 'dark' ? 'white' : 'rgb(33,37,41)',
+                }}
+            >
+                <h2>{props.heading}</h2>
+                <div className="mb-3">
+                    <label htmlFor="myBox" className="form-label">
+                        Enter your Text below:
+                    </label>
+                    <textarea
+                        className="form-control"
+                        id="myBox"
+                        rows="8"
+                        placeholder="Enter your text here..."
+                        value={text}
+                        onChange={changeHandler}
+                        style={{
+                            backgroundColor:
+                                props.mode === 'dark'
+                                    ? 'rgb(73,77,71)'
+                                    : 'white',
+                            color:
+                                props.mode === 'dark'
+                                    ? 'white'
+                                    : 'rgb(33,37,41)',
+                        }}
+                    ></textarea>
+                </div>
+                <button
+                    className="btn btn-primary mx-2"
+                    onClick={handleUpClick}
+                >
+                    Convert to Uppercase
+                </button>
+                <button
+                    className="btn btn-primary mx-2"
+                    onClick={handleLowClick}
+                >
+                    Convert to Lowercase
+                </button>
+                <button
+                    className="btn btn-danger mx-2"
+                    onClick={handleClearClick}
+                >
+                    Clear
+                </button>
+                <button className="btn btn-primary mx-2" onClick={handleCopy}>
+                    Copy Text
+                </button>
+                <button
+                    className="btn btn-primary mx-2"
+                    onClick={handleExtraSpaces}
+                >
+                    Remove Extra Spaces
+                </button>
+                <button
+                    className="btn btn-primary mx-2"
+                    onClick={handleRemoveSpaces}
+                >
+                    Remove All Spaces
+                </button>
+            </div>
+            <div
+                className="container my-3 py-3"
+                style={{
+                    backgroundColor:
+                        props.mode === 'dark' ? 'rgb(33,37,41)' : 'white',
+                    color: props.mode === 'dark' ? 'white' : 'rgb(33,37,41)',
+                }}
+            >
+                <h2>Summary:</h2>
+                <p>
+                    {text.split(' ').length} words and {text.length} characters.
+                </p>
+                <p>Read in {(1 / 125) * text.split(' ').length} minutes</p>
+                <h4>Preview:</h4>
+                <p>{text}</p>
+            </div>
+        </React.Fragment>
+    );
+}
+
+export default TextForm;
